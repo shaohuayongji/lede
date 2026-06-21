3019cecb153bba156e7a271e4d50f8fb9763bd8f
shaohuayongji <sy61008317@gmail.com>
2026-06-21 12:02:40 +0800

    Add custom LuCI packages to Actions builds

---
+++ scripts/prepare-custom-luci-apps.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+WORK_DIR="${RUNNER_TEMP:-/tmp}/lede-custom-luci-apps"
+LEAN_LUCI_REF="${LEAN_LUCI_REF:-master}"
+ISTORE_REF="${ISTORE_REF:-main}"
+
+rm -rf "${WORK_DIR}"
+mkdir -p "${WORK_DIR}"
+
+echo "Fetching luci-app-filetransfer from coolsnowwolf/luci@${LEAN_LUCI_REF}"
+git clone --depth 1 --branch "${LEAN_LUCI_REF}" https://github.com/coolsnowwolf/luci.git "${WORK_DIR}/lean-luci"
+
+mkdir -p "${ROOT_DIR}/feeds/luci/applications" "${ROOT_DIR}/feeds/luci/libs"
+rm -rf \
+  "${ROOT_DIR}/feeds/luci/applications/luci-app-filetransfer" \
+  "${ROOT_DIR}/feeds/luci/libs/luci-lib-fs"
+
+cp -a "${WORK_DIR}/lean-luci/applications/luci-app-filetransfer" "${ROOT_DIR}/feeds/luci/applications/"
+cp -a "${WORK_DIR}/lean-luci/libs/luci-lib-fs" "${ROOT_DIR}/feeds/luci/libs/"
+
+echo "Fetching luci-app-store from linkease/istore@${ISTORE_REF}"
+git clone --depth 1 --branch "${ISTORE_REF}" https://github.com/linkease/istore.git "${WORK_DIR}/istore"
+
+mkdir -p "${ROOT_DIR}/package/custom"
+for package_name in luci-app-store luci-lib-taskd luci-lib-xterm taskd; do
+  rm -rf "${ROOT_DIR}/package/custom/${package_name}"
+  cp -a "${WORK_DIR}/istore/luci/${package_name}" "${ROOT_DIR}/package/custom/"
+done
+
+echo "Custom LuCI packages are ready."
