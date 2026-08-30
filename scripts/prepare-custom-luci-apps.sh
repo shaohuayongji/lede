e7d0c401039cc93603f53ab2c61566733f390157
shaohuayongji <sy61008317@gmail.com>
2026-08-30 08:13:56 +0800

    Replace Alist with OpenList2 LuCI

--- scripts/prepare-custom-luci-apps.sh
+++ scripts/prepare-custom-luci-apps.sh
@@ -5,6 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 WORK_DIR="${RUNNER_TEMP:-/tmp}/lede-custom-luci-apps"
 LEAN_LUCI_REF="${LEAN_LUCI_REF:-master}"
 ISTORE_REF="${ISTORE_REF:-main}"
+OPENLIST_REF="${OPENLIST_REF:-main}"
 
 rm -rf "${WORK_DIR}"
 mkdir -p "${WORK_DIR}"
@@ -27,6 +28,14 @@ mkdir -p "${ROOT_DIR}/package/custom"
 for package_name in luci-app-store luci-lib-taskd luci-lib-xterm taskd; do
   rm -rf "${ROOT_DIR}/package/custom/${package_name}"
   cp -a "${WORK_DIR}/istore/luci/${package_name}" "${ROOT_DIR}/package/custom/"
+done
+
+echo "Fetching luci-app-openlist2 from sbwml/luci-app-openlist2@${OPENLIST_REF}"
+git clone --depth 1 --branch "${OPENLIST_REF}" https://github.com/sbwml/luci-app-openlist2.git "${WORK_DIR}/openlist2"
+
+for package_name in openlist2 luci-app-openlist2; do
+  rm -rf "${ROOT_DIR}/package/custom/${package_name}"
+  cp -a "${WORK_DIR}/openlist2/${package_name}" "${ROOT_DIR}/package/custom/"
 done
 
 echo "Custom LuCI packages are ready."
